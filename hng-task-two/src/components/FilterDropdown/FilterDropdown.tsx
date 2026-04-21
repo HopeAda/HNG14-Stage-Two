9ed45b407ba9c73df60f47cbc6313450ab8348aa
Adama Asenath <169082427+HopeAda@users.noreply.github.com>
2026-04-21 16:27:33 +0100

    I created logic for opening and closing dropdown and for selecting filters

--- hng-task-two/src/components/FilterDropdown/FilterDropdown.tsx
+++ hng-task-two/src/components/FilterDropdown/FilterDropdown.tsx
@@ -1,10 +1,55 @@
 import "./FilterDropdown.css";
 import Caret from "../../assets/caret-down.svg";
+import { useState, useRef, useEffect } from "react";
+
+type Filter = {
+	draft: boolean;
+	pending: boolean;
+	paid: boolean;
+};
 
 const FilterDropdown = () => {
+	const [filterConditions, setFilterConditions] = useState<Filter>({
+		draft: false,
+		pending: false,
+		paid: false,
+	});
+
+	const filterHandler = (condition: keyof Filter) => {
+		setFilterConditions((prev) => ({
+			...prev,
+			[condition]: !prev[condition],
+		}));
+	};
+
+	const dropdownRef = useRef<HTMLDivElement | null>(null);
+
+	const [filterOpen, setFilterOpen] = useState(false);
+
+	useEffect(() => {
+		const handleOutsideClick = (e: MouseEvent) => {
+			if (
+				dropdownRef.current &&
+				!dropdownRef.current.contains(e.target as Node)
+			) {
+				setFilterOpen(false);
+			}
+		};
+		document.addEventListener("mousedown", handleOutsideClick);
+
+		return () => {
+			document.removeEventListener("mousedown", handleOutsideClick);
+		};
+	}, []);
+
 	return (
-		<div className="filter">
-			<button className="display">
+		<div className={`filter ${filterOpen ? "open" : ""}`} ref={dropdownRef}>
+			<button
+				className="display"
+				onClick={() => {
+					setFilterOpen((prev) => !prev);
+				}}
+			>
 				<span className="display-name">Filter by status</span>
 				<span className="img-cont">
 					<img src={Caret} alt="" />
@@ -13,18 +58,36 @@ const FilterDropdown = () => {
 			<div className="filter-list">
 				<ul>
 					<li>
-						<input type="checkbox" name="draft" id="draft" />
-						<span>Draft</span>
+						<input
+							type="checkbox"
+							name="draft"
+							id="draft"
+							checked={filterConditions.draft}
+							onChange={() => filterHandler("draft")}
+						/>
+						<label htmlFor="draft">Draft</label>
 					</li>
 
 					<li>
-						<input type="checkbox" name="pending" id="pending" />
-						<span>Pending</span>
+						<input
+							type="checkbox"
+							name="pending"
+							id="pending"
+							checked={filterConditions.pending}
+							onChange={() => filterHandler("pending")}
+						/>
+						<label htmlFor="pending">Pending</label>
 					</li>
 
 					<li>
-						<input type="checkbox" name="paid" id="paid" />
-						<span>Paid</span>
+						<input
+							type="checkbox"
+							name="paid"
+							id="paid"
+							checked={filterConditions.paid}
+							onChange={() => filterHandler("paid")}
+						/>
+						<label htmlFor="paid">Paid</label>
 					</li>
 				</ul>
 			</div>
