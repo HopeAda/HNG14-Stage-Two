00fbbee30d8fc3a97dafd312348879cf92381a13
Adama Asenath <169082427+HopeAda@users.noreply.github.com>
2026-04-22 17:31:03 +0100

    Added conditional routing for add form page

--- hng-task-two/src/App.tsx
+++ hng-task-two/src/App.tsx
@@ -1,17 +1,28 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 import Details from "./pages/Details/Details";
+import Add from "./pages/Add/Add";
 
 const App = () => {
+	const location = useLocation();
+	const state = location.state as { backgroundLocation?: Location };
+
 	return (
 		<div className="app">
 			<Navbar />
-			<Routes>
+			<Routes location={state?.backgroundLocation || location}>
 				<Route element={<Home />} path="/" />
 				<Route element={<Details />} path="/details/:id" />
+				<Route element={<Add />} path="/add" />
 			</Routes>
+
+			{state?.backgroundLocation && (
+				<Routes>
+					<Route element={<Add />} path="/add" />
+				</Routes>
+			)}
 		</div>
 	);
 };
