f4f2d4e04557a12c09407574701172cdf2109636
Adama Asenath <169082427+HopeAda@users.noreply.github.com>
2026-04-22 02:55:08 +0100

    Created a details page and components for buttons and the total price banner

---
+++ hng-task-two/src/pages/Details/Details.tsx
@@ -0,0 +1,77 @@
+import "./Details.css";
+import Back from "../../assets/back.svg";
+import StatusBadge from "../../components/StatusBadge/StatusBadge";
+import Banner from "../../components/Banner/Banner";
+import Button from "../../components/Button/Button";
+
+const Details = () => {
+	return (
+		<div className="details-page">
+			<button className="back">
+				<img src={Back} alt="" />
+				<span>Go back</span>
+			</button>
+
+			<section className="status">
+				<div className="status-info">
+					<p>Status</p>
+					<StatusBadge />
+				</div>
+				<div className="buttons">
+					<Button name="edit" />
+					<Button name="delete" />
+					<Button name="Mark as Paid" />
+				</div>
+			</section>
+
+			<section className="invoice-details">
+				<div className="invoice-info">
+					<span className="id">
+						#<span className="id-name">XM9141</span>
+					</span>
+					<span className="description small-muted-text">
+						Graphic Design
+					</span>
+				</div>
+
+				<div className="sender-address small-muted-text">
+					<span>19 Union Terrace</span>
+					<span>London</span>
+					<span>E1 3EZ</span>
+					<span>United Kingdom</span>
+				</div>
+
+				<div className="invoice-date">
+					<span className="small-muted-text">Invoice Date</span>
+					<span className="large-white-text">21 Aug 2021</span>
+				</div>
+
+				<div className="payment-due">
+					<span className="small-muted-text">Payment Due</span>
+					<span className="large-white-text">20 Sep 2021</span>
+				</div>
+
+				<div className="receiver-details small-muted-text">
+					<p>Bill To</p>
+					<span className="receiver-name large-white-text">
+						Alex Grim
+					</span>
+					<div className="receiver-address">
+						<span>84 Church Way</span>
+						<span>Bradford</span>
+						<span>BD1 9PB</span>
+						<span>United Kingdom</span>
+					</div>
+				</div>
+
+				<div className="receiver-email ">
+					<span className="small-muted-text">Sent to</span>
+					<span className="large-white-text">alexgrim@mail.com</span>
+				</div>
+				<Banner />
+			</section>
+		</div>
+	);
+};
+
+export default Details;
