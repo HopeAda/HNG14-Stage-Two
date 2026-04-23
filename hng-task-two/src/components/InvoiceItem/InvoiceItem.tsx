5402a534d89f0932a6b14eac56740a4920b0ae61
Adama Asenath <169082427+HopeAda@users.noreply.github.com>
2026-04-23 12:43:05 +0100

    Passed item data as props to invoice item and itemlist components

--- hng-task-two/src/components/InvoiceItem/InvoiceItem.tsx
+++ hng-task-two/src/components/InvoiceItem/InvoiceItem.tsx
@@ -1,18 +1,23 @@
 import StatusBadge from "../StatusBadge/StatusBadge";
 import "./InvoiceItem.css";
 import Caret from "../../assets/caret-down.svg";
+import type { Invoice } from "../../Context/DataContext";
 
-const InvoiceItem = () => {
+type ItemProviderProps = {
+	data: Invoice;
+};
+
+const InvoiceItem = ({ data }: ItemProviderProps) => {
 	return (
 		<article className="invoice-item">
 			<div className="id">
-				#<span>RT3080</span>
+				#<span>{data.id}</span>
 			</div>
-			<div className="username">Jensen Huang</div>
+			<div className="username">{data.client.name}</div>
 			<span className="due-date">Due 19 Aug 2021</span>
 			<span className="price">€ 1,800.90</span>
 			<div className="extra">
-				<StatusBadge />
+				<StatusBadge type={data.status} />
 				<img src={Caret} className="open-icon" />
 			</div>
 		</article>
