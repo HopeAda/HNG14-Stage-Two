5b722fdbacf64db96093700336bfd9ea380f4de7
Adama Asenath <169082427+HopeAda@users.noreply.github.com>
2026-04-24 00:25:44 +0100

    commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import InvoiceRoutes from "./routes/invoiceRoutes";
+import InvoiceRoutes from "./routes/invoiceRoutes.js";
 
 dotenv.config();
 
